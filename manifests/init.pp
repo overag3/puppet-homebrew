f7909a5fee06baefecea43c6e5cb3c8389d9f45a
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2025-01-20 10:14:57 +0100

    fix: fact compat puppet 8
    - ensure file for file resource

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,7 @@ class homebrew (
   $group                      = 'admin',
   $multiuser                  = false,
 ) {
-
-  if $::operatingsystem != 'Darwin' {
+  if $facts['os']['name'] != 'Darwin' {
     fail('This Module works on Mac OSX only!')
   }
 
@@ -15,19 +14,18 @@ class homebrew (
     fail('Homebrew does not support installation as the "root" user.')
   }
 
-  class { '::homebrew::compiler': }
-  -> class { '::homebrew::install': }
+  class { 'homebrew::compiler': }
+  -> class { 'homebrew::install': }
 
-  contain '::homebrew::compiler'
-  contain '::homebrew::install'
+  contain 'homebrew::compiler'
+  contain 'homebrew::install'
 
   if $homebrew::github_token {
-    file { '/etc/environment': ensure => present }
+    file { '/etc/environment': ensure => file }
     -> file_line { 'homebrew-github-api-token':
       path  => '/etc/environment',
       line  => "HOMEBREW_GITHUB_API_TOKEN=${homebrew::github_token}",
       match => '^HOMEBREW_GITHUB_API_TOKEN',
     }
   }
-
 }
